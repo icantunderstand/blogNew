322783e6f5af88feb551260d85f4941c13ee51d7
sunhao12 <sunhao12@kuaishou.com>
2024-05-10 14:53:27 +0800

    feat: 更新url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'noodles的笔记本',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://icantunderstand.github.io/blogNew/',
   siteRepo: 'https://github.com/icantunderstand/blogNew',
   siteLogo: '/static/images/logo.png',
   socialBanner: '',
