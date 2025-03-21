c3630806231b864d39b6a2f7aad17c8169faea08
sunhao12 <sunhao12@kuaishou.com>
2025-03-21 17:30:27 +0800

    feat: 学习

--- draft/test.js
+++ draft/test.js
@@ -1,19 +1,5 @@
-const merge = (intervals) => {
-    const res = []
-    intervals.sort((a,b) => a[0] - b[0])
-
-    let prev = intervals[0]
-    for(let i = 1; i < intervals.length;i++) { 
-        const curr = intervals[i]
-
-        if(curr[0] <= prev[1]) {
-            prev[1] = Math.max(prev[1], curr[1])
-        } else {
-            res.push(prev)
-            prev = curr
-        }
-    }
-    res.push(prev)
-    
-    
-}
+路径解析
+加载
+包装 
+执行 
+缓存
