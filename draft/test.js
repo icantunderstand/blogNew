3b69d3c67bbaeebde09096801cad37c02d096041
sunhao12 <sunhao12@kuaishou.com>
2025-04-21 14:46:11 +0800

    feat: learn

---
+++ draft/test.js
@@ -0,0 +1,12 @@
+function curry(fn) {
+
+
+    return function curried(...args) {
+        if(args.length >= fn.length) {
+            return fn.apply(this, args)
+        }
+        return function(...moreArgs) {
+           return curried.apply(this, args.concat(moreArgs))
+        }
+    }
+}
