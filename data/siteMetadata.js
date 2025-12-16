1f52ff24e3f88c8a4f78a178c7a9f784944e3864
sunhao12 <sunhao12@kuaishou.com>
2025-12-16 16:56:37 +0800

    feat: 修改位置

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -81,7 +81,7 @@ const siteMetadata = {
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: 'search.json', // path to load documents to search
+      searchDocumentsPath: '/blogNew/search.json', // path to load documents to search
     },
     // provider: 'algolia',
     // algoliaConfig: {
